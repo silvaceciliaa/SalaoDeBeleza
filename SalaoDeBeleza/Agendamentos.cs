7da5043445372fe3a0d38c5668438a26d25b735a
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2022-08-27 14:50:58 -0300

    Update Agendamentos.cs

--- SalaoDeBeleza/Agendamentos.cs
+++ SalaoDeBeleza/Agendamentos.cs
@@ -1,51 +1,20 @@
-﻿
+
 namespace SalaoDeBeleza
 {
     public class Agendamentos
     {
 
         public string Nome { get; set; }
-        public string Horario { get; set; }
+        public int Horario { get; set; }
+        public int OpcaoFuncao { get; set; }
 
-        public void DadosClientes()
+        public void DadosClientes(int horario)
         {
             Console.Write("Digite seu nome: ");
             Nome = Console.ReadLine();
-            Console.WriteLine("Escolha um dos horários para agendar: (a)09:00 - (b)15:00 - (c)18:00");
-            string opcaoHorario = Console.ReadLine();
-
-            if(opcaoHorario == "a")
-            {
-                int nove = 0;
-               
-             if(nove == 3)
-                {
-                    Console.WriteLine("Não existem horários disponíveis.");
-                }
-             else
-                {
-                    Console.WriteLine("Horário marcado com sucesso!");
-                }
-                nove++;
-            }
-            if (opcaoHorario == "b")
-            {
-                int tres = 0;
-                tres++;
-                if (tres == 3)
-                {
-                    Console.WriteLine("Não existem horários disponíveis.");
-                }
-            }
-            if(opcaoHorario == "c")
-            {
-                int seis = 0;
-                seis++;
-                if (seis == 3)
-                {
-                    Console.WriteLine("Não existem horários disponíveis.");
-                }
-            }
+            Horario = 9 + 1;
+            Console.WriteLine("Agendamento feito com sucesso!");
         }
+
     }
 }
