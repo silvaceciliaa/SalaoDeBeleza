791c4d8a0285637e97d6590a7337d8c7efaf222d
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2022-08-27 23:31:14 -0300

    Update Program.cs

--- SalaoDeBeleza/Program.cs
+++ SalaoDeBeleza/Program.cs
@@ -1,18 +1,15 @@
 using SalaoDeBeleza;
 
-Agendamentos[] clientes = new Agendamentos[9];
-bool loop = true, loop2 = true;
+Agendamentos[] clientes = new Agendamentos[10];
+bool loop = true;
 string opcaoMenu1;
-int opcaoMenu2;
 
 for (int i = 0; i < clientes.Length; i++)
 {
     clientes[i] = new Agendamentos();
 }
-clientes[9].OpcaoFuncao = opcaoMenu2;
-while (loop)
+while (loop) ;
 {
-    
     Console.WriteLine("Seja bem-vindo ao salão Silva&Hoegen! Selecione uma das opções do menu:");
     Console.WriteLine(" 1 - Agendamento\n 2 - Retirar agendamento\n 3 - Consultar disponibilidade\n 4 - Sair\n");
     opcaoMenu1 = Console.ReadLine();
@@ -20,51 +17,46 @@ while (loop)
     if (opcaoMenu1 == "1")
     {
         Console.Clear();
-        Console.WriteLine("Selecione uma das opções para agendamento");
-        Console.WriteLine(" 1 - Manicure\n 2 - Design de sobrancelhas\n 3 - Corte de cabelo\n");
-        opcaoMenu2 = Convert.ToInt32(Console.ReadLine());
-
-        if (opcaoMenu2 < 4 && opcaoMenu2 > 0)
+        for (int i = 0; i < clientes.Length; i++)
         {
-            bool disponibilidade = false;
-            for (int i = 0; i < clientes.Length; i++)
-            {
-                if (clientes[i].Nome == null)
-                {
-                    disponibilidade = true;
-                    clientes[i].DadosClientes(i);
-                    break;
-                }
-            }
-            for(int i = 0; i < clientes.Length; i++)
-            { 
-                if (disponibilidade == false)
-                {
-                    Console.WriteLine("Não existem horários disponiveis.");
-                }
-            }
+            clientes[i].Agendar();
         }
     }
-     if (opcaoMenu1 == "2")
-        {
+    if (opcaoMenu1 == "2")
+    {
+        Console.WriteLine("Digite seu nome");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Digite o nome da funcao que deseja remover");
+        string funcao = Console.ReadLine();
 
+        for (int i = 0; i < clientes.Length; i++)
+        {
+            if (nome == clientes[i].Nome && funcao == clientes[i].Funcao)
+            {
+                clientes[i].LimparDados();
+            }
+            else
+            {
+                Console.WriteLine("Você não foi encontrado no nosso sistema");
+            }
 
         }
-        if (opcaoMenu1 == "3")
+    }
+    if (opcaoMenu1 == "3")
         {
-        Console.Clear();
-           for(int i = 0; i < clientes.Length; i++)
+            Console.Clear();
+            for (int i = 0; i < clientes.Length; i++)
             {
-                Console.WriteLine($"Nome: {clientes[i].Nome} - Horário: - ");
+                Console.WriteLine($"Nome: {clientes[i].Nome} - Horário: {clientes[i].Horario}:00 - {clientes[i].Funcao}");
             }
         }
         if (opcaoMenu1 == "4")
         {
             Console.WriteLine("Obrigada por usar o programa do Salão Silva&Hoegen!");
-            break;
+            loop = false;
         }
         else
         {
             loop = true;
         }
-    }
+}
